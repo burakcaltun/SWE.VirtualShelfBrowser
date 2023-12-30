aa95201d36d2af7e0b975f515d1acdd6d06d78c1
RIPBurak <burak.yilmaz@2ag.com.tr>
2023-12-30 17:43:20 +0300

    Book düzenleme

---
+++ aspnet-core/src/SWE.VirtualShelfBrowser.Application.Contracts/Books/GetBookListDto.cs
@@ -0,0 +1,8 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace SWE.VirtualShelfBrowser.Books;
+
+public class GetBookListDto : PagedAndSortedResultRequestDto
+{
+    public string? Filter { get; set; }
+}
---
+++ aspnet-core/src/SWE.VirtualShelfBrowser.Domain/Books/BookManager.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace SWE.VirtualShelfBrowser.Books
+{
+    public class BookManager : DomainService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookManager(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<Book> CreateAsync(
+            [NotNull] string name,
+            BookType type,
+            [CanBeNull] DateTime publishDate,
+            Location physicalLocation,
+            string description,
+            string coverImage,
+            int numberOfPage,
+            Guid authorId)
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+
+            return new Book(
+                GuidGenerator.Create(),
+                name,
+                type,
+                publishDate,
+                physicalLocation,
+                description,
+                coverImage,
+                numberOfPage,
+                authorId
+            );
+        }
+
+    }
+}
