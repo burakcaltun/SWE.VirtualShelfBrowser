2a891e157a8494285ccdce889cfdfb3dc3d0e4e6
Burak Can ALTUNOGLU <burakcanaltunoglu@gmail.com>
2024-01-01 14:35:19 +0300

    book-lending appservice

--- aspnet-core/src/SWE.VirtualShelfBrowser.Application.Contracts/Lendings/ILendingAppService.cs
+++ aspnet-core/src/SWE.VirtualShelfBrowser.Application.Contracts/Lendings/ILendingAppService.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using SWE.VirtualShelfBrowser.Books;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
 namespace SWE.VirtualShelfBrowser.Lendings
 {
-    public interface ILendingAppService : IApplicationService
+    public interface ILendingAppService :
+    ICrudAppService< //Defines CRUD methods
+        LendingDto, //Used to show books
+        Guid, //Primary key of the book entity
+        PagedAndSortedResultRequestDto, //Used for paging/sorting
+        CreateUpdateLendingDto> //Used to create/update a book
     {
+        // ADD the NEW METHOD
+        Task<ListResultDto<BookLookupDto>> GetBookLookupAsync();
     }
 }
